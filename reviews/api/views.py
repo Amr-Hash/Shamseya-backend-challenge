708a8126688f5c4a5d0abc1005c9a74b634f7ae5
Amr2012Hashem <amr.hashem@t3.com.eg>
2021-01-02 01:31:11 +0200

    auth - premission - filter

--- reviews/api/views.py
+++ reviews/api/views.py
@@ -14,9 +14,20 @@ class IsStaffOrSuperUser(BasePermission):
 class DayViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = DaySerializer
     queryset = Day.objects.prefetch_related('reviews').all()
+
+    authentication_classes = [BasicAuthentication, SessionAuthentication]
+    permission_classes = [IsStaffOrSuperUser]
+
     def get_queryset(self):
         grand_children = Prefetch('reviews__answers', queryset=Answer.objects.select_related('review','question','choice'))
         queryset = Day.objects.prefetch_related(grand_children)
+
+        from_date = self.request.query_params.get('from', None)
+        to_date = self.request.query_params.get('to', None)
+        if from_date:
+            self.queryset = self.queryset.filter(date__gte=from_date)
+        if to_date:
+            self.queryset = self.queryset.filter(date__lte=to_date)
         return queryset
     @query_debugger
     def list(self, request):
